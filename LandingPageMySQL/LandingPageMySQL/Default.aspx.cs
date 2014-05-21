8293746aa435995dbb540f5fa663f84a4fbabeee
Nezir Yürekli <neziryurekli@gmail.com>
2014-05-21 22:36:13 +0300

    Update Default.aspx.cs

--- LandingPageMySQL/LandingPageMySQL/Default.aspx.cs
+++ LandingPageMySQL/LandingPageMySQL/Default.aspx.cs
@@ -14,20 +14,20 @@ namespace LandingPageMySQL
 		public virtual void button1Clicked (object sender, EventArgs args)
 		{
 			string connectionString = "Server=localhost;Database=cdcat;Password=argeset;User ID=root;Pooling=false;";
-    	    MySqlConnection dbcon = new MySqlConnection(connectionString);
-    	    dbcon.Open();
+    	    		MySqlConnection dbcon = new MySqlConnection(connectionString);
+    	    		dbcon.Open();
 
 			MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM users", dbcon);
-    	    DataSet ds = new DataSet();
-    	    adapter.Fill(ds, "result");
+    	    		DataSet ds = new DataSet();
+    	    		adapter.Fill(ds, "result");
 
 			MySqlCommand command = new MySqlCommand();
 			command.Connection = dbcon;
-            command.CommandText=("INSERT INTO users (mail) VALUES ( ' " + txtEmail.Text + " ' )");
-            command.ExecuteNonQuery(); 
+            		command.CommandText=("INSERT INTO users (mail) VALUES ( ' " + txtEmail.Text + " ' )");
+            		command.ExecuteNonQuery(); 
 
 			dbcon.Close();
-    	    dbcon = null;
+    	    		dbcon = null;
 		}
 	}
 }
